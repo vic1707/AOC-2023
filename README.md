6a4c1bd02dc6113d2be5b9b3718f7af483a1068c
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-12-01 18:32:00 +0100

    day 1 -  part 2 - slow solution

--- README.md
+++ README.md
@@ -10,11 +10,11 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 ## Benchmarks
 
-|           Day            |  Part 1  | Part 2 |
-| :----------------------: | :------: | :----: |
-| [Day 1](./src/bin/01.rs) | `71.5µs` |  `-`   |
+|           Day            |  Part 1  | Part 2  |
+| :----------------------: | :------: | :-----: |
+| [Day 1](./src/bin/01.rs) | `71.2µs` | `1.3ms` |
 
-**Total: 0.07ms**
+**Total: 1.37ms**
 
 <!--- benchmarking table --->
 
--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use std::{convert::Infallible, str::FromStr};
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -15,7 +17,60 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    const NUMBER_PATTERNS: &[&str] = &[
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", // 1-9
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", // 1-9
+    ];
+
+    Some(
+        input
+            .lines()
+            .flat_map(|line| {
+                let mut find_first = NUMBER_PATTERNS
+                    .iter()
+                    .map(|pattern| (pattern, line.find(pattern)))
+                    .filter(|(_, index)| index.is_some())
+                    .collect::<Vec<_>>();
+                find_first.sort_by(|(_, a), (_, b)| a.cmp(b));
+                let first = find_first.first().unwrap();
+                let mut find_last = NUMBER_PATTERNS
+                    .iter()
+                    .map(|pattern| (pattern, line.rfind(pattern)))
+                    .filter(|(_, index)| index.is_some())
+                    .collect::<Vec<_>>();
+                find_last.sort_by(|(_, a), (_, b)| a.cmp(b));
+                let last = find_last.last().unwrap();
+
+                format!(
+                    "{}{}",
+                    first.0.parse::<Num>().unwrap().0,
+                    last.0.parse::<Num>().unwrap().0
+                )
+                .parse::<u32>()
+            })
+            .sum(),
+    )
+}
+
+struct Num(u32);
+
+impl FromStr for Num {
+    type Err = Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "1" | "one" => Num(1),
+            "2" | "two" => Num(2),
+            "3" | "three" => Num(3),
+            "4" | "four" => Num(4),
+            "5" | "five" => Num(5),
+            "6" | "six" => Num(6),
+            "7" | "seven" => Num(7),
+            "8" | "eight" => Num(8),
+            "9" | "nine" => Num(9),
+            _ => panic!("Invalid number"),
+        })
+    }
 }
 
 #[cfg(test)]
@@ -34,9 +89,13 @@ mod tests {
 
     #[test]
     fn test_part_two() {
+        // example
         let result = part_two(&advent_of_code::template::read_file_part(
             "examples", DAY, 2,
         ));
         assert_eq!(result, Some(281));
+        // real input
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(54925));
     }
 }
