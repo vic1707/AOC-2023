53d4d02eb711f04006644ce31fe8089065848672
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-12-01 17:56:28 +0100

    day 1 -  part 1 - test real file

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -24,8 +24,12 @@ mod tests {
 
     #[test]
     fn test_part_one() {
+        // example
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(142));
+        // real input
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(55172));
     }
 
     #[test]
