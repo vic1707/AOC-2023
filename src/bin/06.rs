b4c071eef7056402d5e584764b2f438907c1fc9e
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-12-06 15:21:38 +0100

    day 6 - part 1 - solution

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,7 +1,26 @@
 advent_of_code::solution!(6);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let (times, distances) = input.split_once('\n').unwrap();
+
+    let time = times
+        .split_whitespace()
+        .skip(1)
+        .map(|s| s.bytes().fold(0, |acc, f| acc * 10 + (f - b'0') as u32));
+
+    let distances = distances
+        .split_whitespace()
+        .skip(1)
+        .map(|s| s.bytes().fold(0, |acc, f| acc * 10 + (f - b'0') as u32));
+
+    Some(
+        time.zip(distances)
+            .map(|(t, d)| {
+                let x1 = (t - ((t * t - (d << 2)) as f32).sqrt() as u32) / 2;
+                t - 2 * x1 + 1 - 2 * u32::from(x1 * (t - x1) <= d)
+            })
+            .product(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
