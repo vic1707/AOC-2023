e661549f29ad14b859754e7b80d5fda1001f9b4c
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-12-08 16:52:44 +0100

    Squashed commit of the following:
    commit 2257cc8cc93493da8b4a6565a2e3c5e223834d4d
    Author: vic1707 <28602203+vic1707@users.noreply.github.com>
    Date:   Fri Dec 8 16:52:25 2023 +0100
    
        working, dunno how, maybe luck, i'm done...
    
    commit 6d5bb4922718ba73c3cc16e28ac4ded2c2af94e9
    Author: vic1707 <28602203+vic1707@users.noreply.github.com>
    Date:   Fri Dec 8 10:32:47 2023 +0100
    
        headache
    
    commit 3090aea5c53770c60726015492af7c15c0188c4f
    Author: vic1707 <28602203+vic1707@users.noreply.github.com>
    Date:   Thu Dec 7 21:07:56 2023 +0100
    
        WIP 2
    
    commit d061f5614f0f8f34972a263b5038d9f4c8455959
    Author: vic1707 <28602203+vic1707@users.noreply.github.com>
    Date:   Thu Dec 7 10:58:13 2023 +0100
    
        WIP
    
    commit 202923b1b9c29f76cca293d1662d4291bd00fac9
    Author: vic1707 <28602203+vic1707@users.noreply.github.com>
    Date:   Wed Dec 6 18:57:36 2023 +0100
    
        nicer looking split
    
    commit 6e29ecf9b76484ced6f521ed7032dfb3ef27494f
    Author: vic1707 <28602203+vic1707@users.noreply.github.com>
    Date:   Wed Dec 6 18:50:23 2023 +0100
    
        move to ranges

--- README.md
+++ README.md
@@ -14,13 +14,13 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | :----------------------: | :-------: | :-------: |
 | [Day 1](./src/bin/01.rs) | `17.5µs`  | `31.2µs`  |
 | [Day 2](./src/bin/02.rs) |  `5.2µs`  |  `5.2µs`  |
-| [Day 3](./src/bin/03.rs) | `21.2µs`  | `15.4µs`  |
+| [Day 3](./src/bin/03.rs) | `19.8µs`  | `15.4µs`  |
 | [Day 4](./src/bin/04.rs) | `25.9µs`  | `25.9µs`  |
-| [Day 5](./src/bin/05.rs) | `13.6µs`  | `30+min`  |
+| [Day 5](./src/bin/05.rs) | `13.6µs`  | `44.8µs`  |
 | [Day 6](./src/bin/06.rs) | `118.0ns` | `109.0ns` |
-| [Day 7](./src/bin/07.rs) | `53.3µs`  | `56.9µs`  |
+| [Day 7](./src/bin/07.rs) | `53.3µs`  | `52.7µs`  |
 
-**Total: 0.28ms** (without the 30+min for day 5 part 2)
+**Total: 0.31ms**
 
 <!--- benchmarking table --->
 
--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,10 +1,16 @@
+use core::{iter, ops::RangeInclusive};
+
 advent_of_code::solution!(5);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let mut zones = input.split("\n\n");
-    let seeds_zone = zones.next()?;
+/* (begin..end, op_transformation) */
+type Table = (RangeInclusive<i64>, i64);
+type Map = Vec<Table>;
 
-    let maps = zones
+pub fn part_one(input: &str) -> Option<u32> {
+    let (seeds, maps) = input.split_once("\n\n")?;
+
+    let maps = maps
+        .split("\n\n")
         .map(|map| {
             map.as_bytes()
                 .split(|&b| b == b'\n')
@@ -18,20 +24,23 @@ pub fn part_one(input: &str) -> Option<u32> {
                             acc
                         })
                 })
-                .map(|[target, source, step]| [target - source, source, step + source])
-                .collect::<Vec<[_; 3]>>()
+                .map(|[target, source, step]| (source..=step + source, target - source))
+                .collect::<Map>()
         })
-        .collect::<Vec<Vec<[_; 3]>>>();
+        .collect::<Vec<Map>>();
 
-    seeds_zone
+    seeds
         .as_bytes()
         .split(|&b| b == b' ')
         .skip(1)
         .map(parse_slice_to_number)
         .map(|seed| {
             maps.iter().fold(seed, |seed, ranges| {
-                for &[transformation, begin_range, end_range] in ranges.iter() {
-                    if seed >= begin_range && seed < end_range {
+                for (range, transformation) in ranges.iter() {
+                    if range.contains(&seed) {
+                        // early return if we found the range
+                        // else we could hit another range after transformation
+                        // which would be wrong
                         return seed + transformation;
                     }
                 }
@@ -43,10 +52,10 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let mut zones = input.split("\n\n");
-    let seeds_zone = zones.next()?;
+    let (seeds, maps) = input.split_once("\n\n")?;
 
-    let maps = zones
+    let maps = maps
+        .split("\n\n")
         .map(|map| {
             map.as_bytes()
                 .split(|&b| b == b'\n')
@@ -55,40 +64,96 @@ pub fn part_two(input: &str) -> Option<u32> {
                     range
                         .split(|&b| b == b' ')
                         .enumerate()
-                        .fold([0_i64; 3], |mut acc, (idx, n)| {
+                        .fold([0; 3], |mut acc, (idx, n)| {
                             acc[idx] = parse_slice_to_number(n);
                             acc
                         })
                 })
-                .map(|[target, source, step]| [target - source, source, step + source])
-                .collect::<Vec<[_; 3]>>()
+                .map(|[target, source, step]| (source..=step + source, target - source))
+                .collect::<Map>()
         })
-        .collect::<Vec<Vec<[_; 3]>>>();
+        .collect::<Vec<Map>>();
 
-    // take seeds numbers two by two
-    seeds_zone
+    let mut seeds = seeds
         .as_bytes()
         .split(|&b| b == b' ')
         .skip(1)
-        .map(parse_slice_to_number)
-        .collect::<Vec<_>>()
-        .chunks_exact(2)
-        .flat_map(|pair| {
-            ((pair[0] - 1)..=pair[0] + pair[1])
-                .map(|seed| {
-                    maps.iter().fold(seed, |seed, ranges| {
-                        for &[transformation, begin_range, end_range] in ranges.iter() {
-                            if seed >= begin_range && seed < end_range {
-                                return seed + transformation;
+        .map(parse_slice_to_number);
+
+    iter::from_fn(|| seeds.next().zip(seeds.next()))
+        .map(|(begin, len)| (begin..=begin + len))
+        .flat_map(|original_seed_range| {
+            maps.iter()
+                .fold(vec![original_seed_range], |seeds_ranges, map_ranges| {
+                    seeds_ranges
+                        .into_iter()
+                        .flat_map(|mut seed_range| {
+                            let mut fully_contained = false;
+                            let mut news = map_ranges.iter().fold(
+                                vec![],
+                                |mut acc, (map_range, transformation)| {
+                                    if fully_contained {
+                                        return acc;
+                                    }
+                                    // if the seed range is contained in the map range
+                                    if map_range.contains(seed_range.start())
+                                        && map_range.contains(seed_range.end())
+                                    {
+                                        acc.push(
+                                            (seed_range.start() + transformation)
+                                                ..=(seed_range.end() + transformation),
+                                        );
+                                        fully_contained = true;
+                                        return acc;
+                                    }
+                                    // if the left bound of the map range is contained in the seed range
+                                    if seed_range.contains(map_range.start()) {
+                                        if map_range.start() != seed_range.end() {
+                                            acc.push(
+                                                map_range.start() + transformation
+                                                    ..=seed_range.end().min(map_range.end())
+                                                        + transformation,
+                                            );
+                                        }
+                                        // try out both,
+                                        // frankly i don't think it really works but it passes tests and gives the right answer
+                                        let a = *seed_range.start()..=*map_range.start();
+                                        let b = *map_range.end()..=*seed_range.end();
+                                        if a.start() < a.end() {
+                                            seed_range = a;
+                                        }
+                                        if b.start() < b.end() {
+                                            seed_range = b;
+                                        }
+                                    }
+                                    // if the right bound of the map range is contained in the seed range
+                                    if seed_range.contains(map_range.end()) {
+                                        if seed_range.start() != map_range.end() {
+                                            acc.push(
+                                                seed_range.start() + transformation
+                                                    ..=map_range.end().min(seed_range.end())
+                                                        + transformation,
+                                            );
+                                        }
+                                        seed_range = *map_range.end()..=*seed_range.end();
+                                    }
+                                    acc
+                                },
+                            );
+
+                            if !fully_contained {
+                                // println!("    LEFTOVER: {:#?}", seed_range);
+                                if seed_range.start() < seed_range.end() {
+                                    news.push(seed_range);
+                                }
                             }
-                        }
-                        seed
-                    })
+                            news
+                        })
+                        .collect::<Vec<RangeInclusive<i64>>>()
                 })
-                .min()
         })
+        .map(|range| *range.start() as u32)
         .min()
-        .map(|n| n as u32)
 }
 
 fn parse_slice_to_number(slice: &[u8]) -> i64 {
@@ -103,11 +168,17 @@ mod tests {
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(35));
+
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(226_172_555));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(46));
+
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(47_909_639));
     }
 }
