cadd49b6c46ba8ecc1f539769d6768e45d71cff2
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-12-08 16:54:13 +0100

    rm d5 tests on real inputs

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -168,17 +168,11 @@ mod tests {
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(35));
-
-        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
-        assert_eq!(result, Some(226_172_555));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(46));
-
-        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
-        assert_eq!(result, Some(47_909_639));
     }
 }
